e956faa98a25684064cf006331966dcd7b1198da
cavinalopez <cavinalopez@cpp.edu>
2019-08-18 21:15:54 -0700

    first commit, AWS credentials need to be modified to allow file uploads to S3 account

---
+++ src/main/java/com/example/controller/AmazonClient.java
@@ -0,0 +1,95 @@
+package com.example.controller;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+
+@Service
+@ConfigurationProperties
+public class AmazonClient {
+
+    private AmazonS3 s3Client;
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+
+    @Value("${amazonProperties.regionName}")
+    private String regionName;
+
+    @PostConstruct
+    private void initializeAmazon(){
+        System.out.println("access key is: " + this.accessKey);
+        System.out.println("secret key is: " + this.secretKey);
+
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(regionName)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    private File convertMultiPartFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+
+        fos.write(file.getBytes());
+        fos.close();
+
+        return convFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart){
+        return new Date().getTime() + "_" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file){
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, file).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String uploadFile(MultipartFile multipartFile){
+
+        String fileUrl = "";
+        try{
+            File file = convertMultiPartFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return fileUrl;
+    }
+
+    public String deleteFileFromS3Bucket(String fileUrl) {
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        s3Client.deleteObject(new DeleteObjectRequest(bucketName + "/", fileName));
+        return "Successfully deleted";
+    }
+}
